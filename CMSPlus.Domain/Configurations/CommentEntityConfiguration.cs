426b22e5cb67ff79e319f66fef04320291ab29b1
Ion <ion.borta111@mail.ru>
2024-11-23 01:00:41 +0200

    Added and configured Comment Entity

---
+++ CMSPlus.Domain/Configurations/CommentEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CMSPlus.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMSPlus.Domain.Configurations
+{
+    public class CommentEntityConfiguration : IEntityTypeConfiguration<CommentEntity>
+    {
+        public void Configure(EntityTypeBuilder<CommentEntity> builder)
+        {
+            builder.ToTable("Comments");
+            builder.Property(x => x.FullName).IsRequired();
+            builder.Property(x => x.CommentText).IsRequired();
+            builder.Property(x => x.TopicId).IsRequired();
+            builder.Ignore(x => x.UpdatedOnUtc);
+        }
+    }
+}
